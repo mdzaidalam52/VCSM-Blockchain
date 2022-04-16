6329b1d3ad9ad8258726d65fccc3af4fe4afc6c2
mdzaidalam52 <mdzaidalam001@gmail.com>
2022-04-16 14:09:45 +0530

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,107 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 // import SimpleStorageContract from "./contracts/SimpleStorage.json";
 // import getWeb3 from "./getWeb3";
+import Beneficiary from './Beneficiary'
+import * as bst from 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import "./App.css";
+import './App.css'
 
 function App() {
-  // const [values, setValues] = useState({ storageValue: 0, web3: null, accounts: null, contract: null })
-  
-  // const componentDidMount = async () => {
-  //   try {
-  //     // Get network provider and web3 instance.
-  //     const web3 = await getWeb3();
+    // const [values, setValues] = useState({ storageValue: 0, web3: null, accounts: null, contract: null })
 
-  //     // Use web3 to get the user's accounts.
-  //     const accounts = await web3.eth.getAccounts();
+    // const componentDidMount = async () => {
+    //   try {
+    //     // Get network provider and web3 instance.
+    //     const web3 = await getWeb3();
 
-  //     // Get the contract instance.
-  //     const networkId = 5777;
-  //     const deployedNetwork = SimpleStorageContract.networks[networkId];
-  //     const instance = new web3.eth.Contract(
-  //       SimpleStorageContract.abi,
-  //       deployedNetwork && deployedNetwork.address,
-  //     );
+    //     // Use web3 to get the user's accounts.
+    //     const accounts = await web3.eth.getAccounts();
 
-  //     // Set web3, accounts, and contract to the state, and then proceed with an
-  //     // example of interacting with the contract's methods.
-  //     setValues({...values, contract: instance, accounts, web3});
-  //   } catch (error) {
-  //     // Catch any errors for any of the above operations.
-  //     alert(
-  //       `Failed to load web3, accounts, or contract. Check console for details.`,
-  //     );
-  //     console.error(error);
-  //   }
-  // };
+    //     // Get the contract instance.
+    //     const networkId = 5777;
+    //     const deployedNetwork = SimpleStorageContract.networks[networkId];
+    //     const instance = new web3.eth.Contract(
+    //       SimpleStorageContract.abi,
+    //       deployedNetwork && deployedNetwork.address,
+    //     );
 
-  // const runExample = async () => {
-  //   const { accounts, contract } = values;
-  //   if(!contract)
-  //     return
-  //   console.log(contract)
+    //     // Set web3, accounts, and contract to the state, and then proceed with an
+    //     // example of interacting with the contract's methods.
+    //     setValues({...values, contract: instance, accounts, web3});
+    //   } catch (error) {
+    //     // Catch any errors for any of the above operations.
+    //     alert(
+    //       `Failed to load web3, accounts, or contract. Check console for details.`,
+    //     );
+    //     console.error(error);
+    //   }
+    // };
 
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(29).send({ from: accounts[0] });
+    // const runExample = async () => {
+    //   const { accounts, contract } = values;
+    //   if(!contract)
+    //     return
+    //   console.log(contract)
 
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
+    //   // Stores a given value, 5 by default.
+    //   await contract.methods.set(29).send({ from: accounts[0] });
 
-  //   // Update state with the result.
-  //   setValues({...values,  storageValue: response });
-  // };
+    //   // Get the value from the contract to prove it worked.
+    //   const response = await contract.methods.get().call();
 
-  
-  // useEffect(runExample, [values])
+    //   // Update state with the result.
+    //   setValues({...values,  storageValue: response });
+    // };
 
-  // if (!values.web3) {
-  //   return <div>Loading Web3, accounts, and contract...</div>;
-  // }
-  return (
-    <div className="App">
-      <h1>Dapp</h1>
-    </div>
-  );
+    // useEffect(runExample, [values])
 
+    // if (!values.web3) {
+    //   return <div>Loading Web3, accounts, and contract...</div>;
+    // }
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar bg='primary' variant='dark'>
+                    <Container>
+                        <Navbar.Brand href='#home'>VSCM</Navbar.Brand>
+                        <Nav className='me-auto'>
+                            <Nav.Link>
+                                <Link to='/beneficiary' className='link'>
+                                    Beneficiary
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to='/admin' className='link'>
+                                    Admin
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to='/manufacturer' className='link'>
+                                    Manufacturer
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to='/delivery' className='link'>
+                                    Delivery
+                                </Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <Container>
+                    <Routes>
+                        <Route
+                            path='beneficiary'
+                            element={<Beneficiary />}
+                            exact
+                        />
+                    </Routes>
+                </Container>
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
